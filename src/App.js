59e9b3da028b3e048f051a8cb90d4239b5c6cb64
Rafael Eyng <rafaeleyng@gmail.com>
2018-04-07 22:08:42 -0300

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,83 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    isPlaying: false,
+    beats: 4,
+    tempo: 120,
+  }
+
+  handleBeatsChange = (e) => {
+    this.setState({
+      beats: e.target.value,
+    })
+  }
+
+  handleTempoChange = (e) => {
+    this.setState({
+      tempo: e.target.value,
+    })
+  }
+
+  handleTogglePlay = () => {
+    const { isPlaying } = this.state
+    this.setState({
+      isPlaying: !isPlaying,
+    })
+  }
+
+  renderTogglePlay = () => {
+    const { isPlaying } = this.state
+    return (
+      <button
+        type="button"
+        onClick={this.handleTogglePlay}
+      >
+        {isPlaying ? 'Pause' : 'Play'}
+      </button>
+    )
+  }
+
+  renderTempo = () => {
+    return (
+      <Fragment>
+        <input
+          type="range"
+          value={this.state.tempo}
+          min="40"
+          max="200"
+          step="1"
+          onChange={this.handleTempoChange}
+        />
+        <p>Tempo: {this.state.tempo}</p>
+      </Fragment>
+    )
+  }
+
+  renderBeats = () => {
+    return (
+      <Fragment>
+        <input
+          type="range"
+          value={this.state.beats}
+          min="2"
+          max="20"
+          onChange={this.handleBeatsChange}
+        />
+        <p>Beats: {this.state.beats}</p>
+      </Fragment>
+    )
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.renderTogglePlay()}
+        <hr/>
+        {this.renderTempo()}
+        <hr/>
+        {this.renderBeats()}
       </div>
     );
   }
